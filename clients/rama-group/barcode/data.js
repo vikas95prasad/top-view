49ccb7dfba25a6d5ef45d2f2200c34472b65059f
Vikas Prasad <vikas.prasad@bigbinary.com>
2022-02-19 11:51:15 +0530

    Removed Demos

---
+++ clients/rama-group/barcode/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-2",
+      "name": "Rama Group - Barcode",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.135035335927647,
+        "pitch": 0.5237850968478863,
+        "fov": 1.3261285179797815
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "rama-group",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
